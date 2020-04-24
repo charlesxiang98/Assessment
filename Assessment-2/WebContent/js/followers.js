919a2da16c7812ec32287172795b46a947275703
Charles <xhb@xhb.home>
2020-04-24 19:41:29 -0400

    assessment

--- Assessment-2/WebContent/js/followers.js
+++ Assessment-2/WebContent/js/followers.js
@@ -9,13 +9,14 @@
 $(document).ready(function(){
 	$(function() {
 		var followerurl = $.getUrlParam('followers');
-		$.getJSON(followerurl, function(jsonDoc){
-			console.log(jsonDoc);
+		$.ajax({url:followerurl
+	}).then(function(data){
+			console.log(data);
 			var table="<thead><tr><th scope='col'>id</th><th scope='col'>name</th><th scope='col'>Profile</th></tr></thead><tbody>";
-			$.each(jsonDoc,function(idx){
-				table+="<tr><th scope='row'>"+jsonDoc[idx].id+"</th><td>"
-				+"<a href='http://localhost:8080/Assessment-2/followers.html?followers="+jsonDoc[idx].followers_url+"'>"+jsonDoc[idx].login+"</a></td><td>"
-				+ "<img src='" + jsonDoc[idx].avatar_url + "' width=50 height=50 />"+"</td></tr>";
+			$.each(data,function(idx){
+				table+="<tr><th scope='row'>"+data[idx].id+"</th><td>"
+				+"<a href='http://localhost:8080/Assessment-2/followers.html?followers="+data[idx].followers_url+"'>"+data[idx].login+"</a></td><td>"
+				+ "<img src='" + data[idx].avatar_url + "' width=50 height=50 />"+"</td></tr>";
 			});
 			table+="</tbody>"
 			document.getElementById("result").innerHTML =table;
--- Assessment-2/WebContent/js/index.js
+++ Assessment-2/WebContent/js/index.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-	$("#getButton").click(function() {
-		$.getJSON("https://api.github.com/users", function(jsonDoc){
+	$("#btn").click(function() {
+		$.ajax({url:"https://api.github.com/users"})
+		.then(function(data){
 			var table="<thead><tr><th scope='col'>id</th><th scope='col'>name</th><th scope='col'>Github link</th></tr></thead><tbody>";
-			$.each(jsonDoc,function(idx){
-				table+="<tr><th scope='row'>"+jsonDoc[idx].id+"</th><td>"
-				+"<a href='http://localhost:8080/Assessment-2/followers.html?followers="+jsonDoc[idx].followers_url+"'>"+jsonDoc[idx].login+"</a></td><td>"
-				+ jsonDoc[idx].url+"</td></tr>";
+			$.each(data,function(idx){
+				table+="<tr><th scope='row'>"+data[idx].id+"</th><td>"
+				+"<a href='http://localhost:8080/Assessment-2/followers.html?followers="+data[idx].followers_url+"'>"+data[idx].login+"</a></td><td>"
+				+ data[idx].url+"</td></tr>";
 			});
 			table+="</tbody>"
 			document.getElementById("result").innerHTML =table;
-		});
+		})
 	
-	});
-});
+	})
+})
